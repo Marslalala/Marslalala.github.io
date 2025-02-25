af6254396fd13c4a27b2aab15fef854f0073f10d
Marslalala <108688341+Marslalala@users.noreply.github.com>
2025-02-25 22:22:47 +0000

    Deploying to gh-pages from @ Marslalala/Marslalala.github.io@e22b48f69f4578a1d2e9f02771b2838356eed288 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,33 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/projects/";
           },
         },{id: "post-a-post-with-image-galleries",
       
