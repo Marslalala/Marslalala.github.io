8624a78a3f0aae71dd60ae0206ac2ea0b2cc0408
Marslalala <108688341+Marslalala@users.noreply.github.com>
2025-03-26 21:58:04 +0000

    Deploying to gh-pages from @ Marslalala/Marslalala.github.io@ade7334209e7f8d84cbe779d3568c0fc5ecd94b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,9 +380,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-petal-image-recognition",
+          title: 'Petal Image Recognition',
+          description: "Flower Classification by Leveraging CNN",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
